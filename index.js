0dd62bc973e052259b6f8cbfce2e8cefbb3a25f9
Leo Martinez <leografx@gmail.com>
2019-06-19 07:17:54 -0400

    pdf month layout

--- index.js
+++ index.js
@@ -4,32 +4,20 @@ const pdfDoc = require('pdfkit');
 
 
 const year = 2020;
-// validate a date
-// console.log(cal.dateIsValid(2, 29, 2019));
-
-
-// console.log(cal.generateMonth(1, 2020, { monthShort: true, dayOfWeekShort: true }));
-// console.log(cal.generateMonth(2, 2020));
-// // usageconsole.log(generateMonth(1, 2019));
-// console.log(cal.generateMonth(2, 2019));
-// console.log(cal.generateMonth(3, 2019));
-// console.log(cal.generateMonth(4, 2019));
-// console.log(cal.generateMonth(5, 2019));
-// console.log(cal.generateMonth(6, 2019));
-// console.log(cal.generateMonth(7, 2019));
-// console.log(cal.generateMonth(8, 2019));
-// console.log(cal.generateMonth(9, 2019));
-// console.log(cal.generateMonth(10, 2019));
-// console.log(cal.generateMonth(11, 2019));
-// console.log(cal.generateMonth(12, 2019));
-
 
 const doc = new pdfDoc({
-    size: [864, 1872]
+    size: [864, 1872],
+    margin: 0
 });
 
 
+
+
 renderYear();
+renderMonth(cal.generateMonth(1, 2020), 0, 792);
+renderMonth(cal.generateMonth(2, 2020), 324 - 54, 792);
+renderMonth(cal.generateMonth(3, 2020), 576 - 36, 792);
+// renderMonth(cal.generateMonth(2, 2020));
 
 writeToFile();
 
@@ -50,6 +38,67 @@ function writeToFile() {
     doc.end();
 }
 
-function renderMonth(month, year) {
-    data = cal.generateMonth(month, year);
+function renderMonth(obj, x, y) {
+
+    doc.fontSize(10);
+    // doc.rect(x + 36, y - 36, 36, 36).stroke();
+    doc.text('S', x + 36, y - 36, { align: 'right', width: 36 });
+    doc.text('M', x + 72, y - 36, { align: 'right', width: 36 });
+    doc.text('T', x + 108, y - 36, { align: 'right', width: 36 });
+    doc.text('W', x + 144, y - 36, { align: 'right', width: 36 });
+    doc.text('T', x + 180, y - 36, { align: 'right', width: 36 });
+    doc.text('F', x + 216, y - 36, { align: 'right', width: 36 });
+    doc.text('S', x + 252, y - 36, { align: 'right', width: 36 });
+
+    doc.fontSize(18);
+
+    doc.text(obj.row1[0], x + 36, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[1], x + 72, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[2], x + 108, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[3], x + 144, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[4], x + 180, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[5], x + 216, y, { align: 'right', width: 36 });
+    doc.text(obj.row1[6], x + 252, y, { align: 'right', width: 36 });
+
+    doc.text(obj.row2[0], x + 36, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[1], x + 72, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[2], x + 108, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[3], x + 144, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[4], x + 180, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[5], x + 216, y + 36, { align: 'right', width: 36 });
+    doc.text(obj.row2[6], x + 252, y + 36, { align: 'right', width: 36 });
+
+    doc.text(obj.row3[0], x + 36, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[1], x + 72, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[2], x + 108, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[3], x + 144, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[4], x + 180, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[5], x + 216, y + 72, { align: 'right', width: 36 });
+    doc.text(obj.row3[6], x + 252, y + 72, { align: 'right', width: 36 });
+
+    doc.text(obj.row4[0], x + 36, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[1], x + 72, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[2], x + 108, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[3], x + 144, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[4], x + 180, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[5], x + 216, y + 108, { align: 'right', width: 36 });
+    doc.text(obj.row4[6], x + 252, y + 108, { align: 'right', width: 36 });
+
+    doc.text(obj.row5[0], x + 36, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[1], x + 72, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[2], x + 108, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[3], x + 144, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[4], x + 180, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[5], x + 216, y + 144, { align: 'right', width: 36 });
+    doc.text(obj.row5[6], x + 252, y + 144, { align: 'right', width: 36 });
+
+    doc.text(obj.row6[0], x + 36, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[1], x + 72, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[2], x + 108, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[3], x + 144, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[4], x + 180, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[5], x + 216, y + 180, { align: 'right', width: 36 });
+    doc.text(obj.row6[6], x + 252, y + 180, { align: 'right', width: 36 });
+
+
 }
